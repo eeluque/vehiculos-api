59327046efa7543afd2e1be8ffd52e4b90b8cfda
eeluque <eduardoluque08@gmail.com>
2026-02-04 09:21:49 -0600

    cors

--- src/app.ts
+++ src/app.ts
@@ -14,7 +14,7 @@ app.use(cors({
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"]
 }));
-app.options("*", (req, res) => {
+app.options("/*", (req, res) => {
     res.header("Access-Control-Allow-Origin", req.headers.origin);
     res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
     res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
