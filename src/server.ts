40701444342e56406dedfaccc34fd00ad8b14bf9
eeluque <eduardoluque08@gmail.com>
2026-02-05 08:51:56 -0600

    configuracion de host y port

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import app from './app';
-const port = process.env.PORT || 3001;
+const port = Number(process.env.PORT) || 3001;
 
-app.listen(port, () => {
+
+app.listen(port, "0.0.0.0", () => {
     console.log(`Example app listening on port ${port}`)
 })
